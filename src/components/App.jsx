6a7fd4ac31c28ee580de229c394043c348c3ea14
DESKTOP-CIAEAFE\melody <chaimelo@usc.edu>
2019-02-13 11:01:34 -0800

    can click button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 // import store from "../store";
 import GraphContainer from "../containers/graphContainer.jsx";
 import Graph from "../containers/graph.jsx";
-import '../style/App.css'
 
 
 class App extends Component{
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "./store";
+import './style/App.css'
+
 
 ReactDOM.render(
   <Provider store = {store}>
